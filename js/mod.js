f0527379687c1307bba9b52d516285dcf4d95e51
Seder3214 <50592797+Seder3214@users.noreply.github.com>
2022-09-13 19:58:21 +0300

    0.65

--- js/mod.js
+++ js/mod.js
@@ -13,8 +13,8 @@ let modInfo = {
 
 // Set your version in num and name
 let VERSION = {
-	num: "0.53",
-	name: "One Layer Tree: Infinities!!!",
+	num: "0.65",
+	name: "One Layer Tree: Planets!",
 }
 
 let changelog = `<h1>Changelog:</h1><br> NO`
